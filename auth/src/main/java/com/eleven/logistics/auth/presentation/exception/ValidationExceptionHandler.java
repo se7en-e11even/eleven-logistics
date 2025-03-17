0e102ec22f51704095233a03061e8f0fef7b1eb4
HAN <hansg0325@gmail.com>
2025-03-17 12:29:43 +0900

    Update ValidationExceptionHandler.java

--- auth/src/main/java/com/eleven/logistics/auth/presentation/exception/ValidationExceptionHandler.java
+++ auth/src/main/java/com/eleven/logistics/auth/presentation/exception/ValidationExceptionHandler.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 @RestController
 public class ValidationExceptionHandler {
 
