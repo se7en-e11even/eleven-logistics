273cf4e94c5ba6aa066d709761f909aeb7f47e67
SeungGyu <seungg8361@naver.com>
2025-03-18 00:20:57 +0900

    임시 커밋

---
+++ slack/src/main/java/com/eleven/logistics/slack/application/dto/SlackDto.java
@@ -0,0 +1,18 @@
+package com.eleven.logistics.slack.application.dto;
+
+import lombok.*;
+
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+public class SlackDto {
+
+    private String username;
+    private String message;
+
+    public static SlackDto create(String username, String message) {
+        return SlackDto.builder()
+                .username(username)
+                .message(message)
+                .build();
+    }
+}
